04ec347afe8b00da98a784f95d0b763dcfe9507b
Steve Birks <steven.birks@enable.com>
2018-11-06 08:39:39 +0000

    Update Api/Queries/AdventQueries.cs

--- Api/Queries/AdventQueries.cs
+++ Api/Queries/AdventQueries.cs
@@ -100,7 +100,7 @@ namespace Api.Queries
             {
                 CompletedP1 = false,
                 CompletedP2 = false,
-                StartedP1 = false,
+                StartedP1 = true,
                 StartedP2 = false,
                 AdventDay = new AdventDayDto { Year = 2015, Day = 13 }
             });
@@ -200,7 +200,206 @@ namespace Api.Queries
                 StartedP2 = false,
                 AdventDay = new AdventDayDto { Year = 2015, Day = 25 }
             });
-
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 1 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 2 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 3 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 4 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 5 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 6 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 7 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 8 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 9 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 10 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 11 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 12 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 13 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 14 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 15 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 16 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 17 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 18 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 19 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 20 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 21 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 22 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 23 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 24 }
+            });
+            adventDaysDto.Add(new AdventStatusDto
+            {
+                CompletedP1 = false,
+                CompletedP2 = false,
+                StartedP1 = false,
+                StartedP2 = false,
+                AdventDay = new AdventDayDto { Year = 2018, Day = 25 }
+            });
             adventDaysDto = adventDaysDto.Where(o => o.AdventDay.Year == year).ToList();
 
             return adventDaysDto;
