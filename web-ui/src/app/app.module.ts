7d8e12aecef444950db2c3d2f818d3ab5eb17cc8
StevenBirks <34234552+StevenBirks@users.noreply.github.com>
2018-12-01 09:49:02 +0000

    nav

--- web-ui/src/app/app.module.ts
+++ web-ui/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RouterModule,
     AppRouterModule,
     BrowserAnimationsModule
-    ],
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
