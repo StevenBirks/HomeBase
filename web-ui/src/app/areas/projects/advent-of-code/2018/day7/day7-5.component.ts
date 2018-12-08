013152b655973c702c512e33d6f233f3d5e71ea8
StevenBirks <34234552+StevenBirks@users.noreply.github.com>
2018-12-08 09:41:34 +0000

    AOC day7 2018

--- web-ui/src/app/areas/projects/advent-of-code/2018/day7/day7-5.component.ts
+++ web-ui/src/app/areas/projects/advent-of-code/2018/day7/day7-5.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-2018-day7-5',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Day7_5_2018Component implements OnInit {
   public inputString: string;
-  public answer: string;
+  public answer: number;
 
   private _steps: iStep[];
   private _stepOrder: string[];
+  private _workers: iWorker[];
+  //private _stepDefs: iStepDef[];
+  private _timeTaken: number;
 
   constructor() { }
 
@@ -19,6 +23,9 @@ export class Day7_5_2018Component implements OnInit {
   public calculate(): void {
     this._steps = new Array<iStep>();
     this._stepOrder = new Array<string>();
+    this._workers = new Array<iWorker>();
+    //this._stepDefs = new Array<iStepDef>();
+    this._timeTaken = 0;
 
     this.inputString.split("\n").forEach((row) => {
 
@@ -42,7 +49,30 @@ export class Day7_5_2018Component implements OnInit {
       }
     });
 
+    // // initialise step defs
+    // let timeRequired = 61;
 
+    // for (let i = 65; i < 91; i++) {
+    //   this._stepDefs.push(<iStepDef> {
+    //     ref: String.fromCharCode(i),
+    //     timeRequired: timeRequired
+    //   })
+    //   timeRequired++;
+    // }
+
+    // initialiase workers
+    for (let i = 0; i < 5; i++) {
+      this._workers.push(<iWorker>{
+        timeRemaining: 0,
+        workingOn: <iStep>{
+          completed: false,
+          ref: "",
+          requires: ""
+        }
+      })
+    }
+
+    // sort steps into order
     this._steps.sort((a, b) => {
       if (a.ref < b.ref) {
         return -1;
@@ -59,7 +89,6 @@ export class Day7_5_2018Component implements OnInit {
       }
     })
 
-
     while (this._steps.filter((step1) => {
       return !step1.completed;
     }).length > 0) {
@@ -73,27 +102,82 @@ export class Day7_5_2018Component implements OnInit {
         });
 
         var foundStepsCompleted = allUncompletedStepsForRef.filter((step4) => {
-          return  this._stepOrder.indexOf(step4.requires) > -1;
+          return this._stepOrder.indexOf(step4.requires) > -1;
         });
 
-
         if (allUncompletedStepsForRef.length === foundStepsCompleted.length) {
-          uncompletedSteps[i].completed = true;
-          if (this._stepOrder.indexOf(uncompletedSteps[i].ref) === -1) {
-            this._stepOrder.push(uncompletedSteps[i].ref);
-          }
-          break;
+          this.farmStep(uncompletedSteps[i]);
         } else {
           if (uncompletedSteps[i].requires === "") {
-            uncompletedSteps[i].completed = true;
-            this._stepOrder.push(uncompletedSteps[i].ref);
-            break;
+            this.farmStep(uncompletedSteps[i]);
           }
         }
       }
+
+      this._timeTaken++;
+      this.iterateTimeRemaining();
     }
 
-    this.answer = this._stepOrder.join("");
+    this.answer = this._timeTaken - 1;
+  }
+
+  private iterateTimeRemaining() {
+    this._workers.forEach((worker) => {
+      if (worker.timeRemaining === 1) {
+        worker.workingOn.completed = true;
+        console.log(`completed: ${worker.workingOn.ref}`);
+        console.log(this._timeTaken);
+
+        if (this._stepOrder.indexOf(worker.workingOn.ref) === -1) {
+          this._stepOrder.push(worker.workingOn.ref);
+          console.log(this._stepOrder);
+        };
+
+        worker.workingOn = <iStep>{
+          completed: false,
+          ref: "",
+          requires: ""
+        };
+      }
+
+      if (worker.timeRemaining > 0) {
+        worker.timeRemaining--;
+      }
+    })
+  }
+
+  private farmStep(potentialStep: iStep) {
+    console.log(`attempt to start ${potentialStep.ref}`)
+    const availableWorkers = this._workers.filter((worker) => {
+      return worker.timeRemaining === 0;
+    });
+
+    const workerWorkingOnStep = this._workers.filter((worker) => {
+      return worker.workingOn.ref === potentialStep.ref;
+    });
+
+    const stepAlreadyCompleted = this._stepOrder.indexOf(potentialStep.ref) > -1;
+
+    if (stepAlreadyCompleted) {
+      potentialStep.completed = true;
+      return;
+    }
+
+    if (workerWorkingOnStep.length === 0) {
+      if (availableWorkers.length > 0) {
+        console.log(`starting step ${potentialStep.ref}`);
+        availableWorkers[0].workingOn = potentialStep;
+        availableWorkers[0].timeRemaining = potentialStep.ref.charCodeAt(0) - 4;
+      }
+      else {
+        console.log(`${potentialStep.ref}:no workers free to work on it`);
+        console.log(this._workers);
+      }
+    }
+    else {
+      console.log(`${potentialStep.ref}: step already working on`);
+      console.log(this._workers);
+    }
   }
 }
 
@@ -102,3 +186,13 @@ interface iStep {
   requires: string,
   completed: boolean
 }
+
+interface iWorker {
+  timeRemaining: number,
+  workingOn: iStep
+}
+
+// interface iStepDef {
+//   ref: string,
+//   timeRequired: number
+// }
