b3cb9d6c01d3cd52bf455d78333ab6cfbaa7b5cb
StevenBirks <34234552+StevenBirks@users.noreply.github.com>
2019-06-12 22:00:13 +0100

    more scale mappings

--- web-ui/src/app/areas/music/violin/violin-scales/violin-scales.component.ts
+++ web-ui/src/app/areas/music/violin/violin-scales/violin-scales.component.ts
@@ -14,26 +14,34 @@ export class ViolinScalesComponent implements OnInit {
 
   ngOnInit() {
     this.keys = new Array<MusicKey>();
-    this._key = "c";
+    this.keys.push({ key: "c", notes: ["c", "d", "e", "f", "g", "a", "b"] });
+    this.keys.push({ key: "c#", notes: ["c#", "d#", "e#", "f#", "g#", "a#", "b#"] });
+    this.keys.push({ key: "db", notes: ["c", "db", "eb", "f", "gb", "ab", "bb"] });
+    this.keys.push({ key: "d", notes: ["c#", "d", "e", "f#", "g", "a", "b"] });
+    this.keys.push({ key: "eb", notes: ["c", "d", "eb", "f", "g", "ab", "bb"] });
+    this.keys.push({ key: "e", notes: ["c#", "d#", "e", "f#", "g#", "a", "b"] });
+    this.keys.push({ key: "f", notes: ["c", "d", "e", "f", "g", "a", "bb"] });
+    this.keys.push({ key: "f#", notes: ["c#", "d#", "e#", "f#", "g#", "a#", "b"] });
+    this.keys.push({ key: "gb", notes: ["cb", "db", "eb", "f", "gb", "ab", "bb"] });
+    this.keys.push({ key: "g", notes: ["c", "d", "e", "f#", "g", "a", "b"] });
+    this.keys.push({ key: "ab", notes: ["c", "db", "eb", "f", "g", "ab", "bb"] });
+    this.keys.push({ key: "a", notes: ["c#", "d", "e", "f#", "g#", "a", "b"] });
+    this.keys.push({ key: "bb", notes: ["c", "d", "eb", "f", "g", "a", "bb"] });
+    this.keys.push({ key: "b", notes: ["c#", "d#", "e", "f#", "g#", "a#", "b"] });
 
-    this.keys.push({key: "c", notes:["c", "d", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "c#", notes:["c", "db", "eb", "f", "g", "ab", "bb"]});
-    this.keys.push({key: "db", notes:["c", "db", "eb", "f", "g", "ab", "bb"]});
-    this.keys.push({key: "d", notes:["c#", "d", "e", "f#", "g", "a", "b"]});
-
-    this.keys.push({key: "d#", notes:["c#", "d#", "e#", "f#", "g#", "a#", "b"]});
-    this.keys.push({key: "e", notes:["c", "d#", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "f", notes:["c", "d#", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "f#", notes:["c", "d#", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "g", notes:["c", "d#", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "g#", notes:["c", "d#", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "a", notes:["c", "d#", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "a#", notes:["c", "d#", "e", "f", "g", "a", "b"]});
-    this.keys.push({key: "b", notes:["c", "d#", "e", "f", "g", "a", "b"]});
+    this.setKey('c');
   }
 
   public setKey(key: string): void {
     this._key = key;
+
+    const element = document.getElementById('key-image');
+    if (element != null) {
+      let className = this._key;
+      className = className.replace('#', '-sharp');
+      element.className = '';
+      element.classList.add(`key-${className}`);
+    }
   };
 
 
