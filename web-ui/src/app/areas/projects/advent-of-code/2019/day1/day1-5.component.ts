dbf7f9f88919377c31bdcedac18db70b92d47949
StevenBirks <34234552+StevenBirks@users.noreply.github.com>
2019-12-01 10:18:22 +0000

    AOC 2019 Day 1

--- web-ui/src/app/areas/projects/advent-of-code/2019/day1/day1-5.component.ts
+++ web-ui/src/app/areas/projects/advent-of-code/2019/day1/day1-5.component.ts
@@ -9,10 +9,40 @@ export class Day1_5_2019Component implements OnInit {
 
   public inputString: string;
   public answer: number;
-
+  private _inputArray: number[];
+  
   ngOnInit() {
   }
 
   public calculate(): void {
+    this.answer = 0;
+    this._inputArray = new Array<number>();
+    this.inputString.split("\n").forEach((value) => {
+      this._inputArray.push(Number.parseInt(value));
+    });
+
+    this._inputArray.forEach((value) => {
+      let fuel = this.calculateFuelForValue(value);
+      let previouslyAddedFuel = fuel;
+      while (previouslyAddedFuel > 0) {
+        const moreFuel = this.calculateFuelForValue(previouslyAddedFuel);
+
+        if (moreFuel > 0) {
+          fuel += moreFuel;
+        }
+
+        previouslyAddedFuel = moreFuel;
+      }
+
+      this.answer += fuel;
+    });
+  }
+
+  private calculateFuelForValue(mass: number):number {
+    let fuel = mass / 3;
+    fuel = Number.parseInt(fuel.toString());
+    fuel -= 2;
+
+    return fuel;
   }
 }
--- web-ui/src/app/areas/projects/advent-of-code/2019/day1/day1.component.ts
+++ web-ui/src/app/areas/projects/advent-of-code/2019/day1/day1.component.ts
@@ -10,10 +10,23 @@ export class Day1_2019Component implements OnInit {
 
   public inputString: string;
   public answer: number
-
+  private _inputArray: number[];
+  
   ngOnInit() {
   }
 
   public calculate(): void {
+    this.answer = 0;
+    this._inputArray = new Array<number>();
+    this.inputString.split("\n").forEach((value) => {
+      this._inputArray.push(Number.parseInt(value));
+    });
+
+    this._inputArray.forEach((value) => {
+      let result = value / 3;
+      result = Number.parseInt(result.toString());
+      result -= 2;
+      this.answer += result;
+    });
   }
 }
